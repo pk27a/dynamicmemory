a758e1bcce0a6937615da0f9b6803aa11ea29f42
pk27a <85279025+pk27a@users.noreply.github.com>
2021-07-28 17:17:37 +0530

    Add files via upload

---
+++ Source Files/Scanned_malloc_only.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+  int n, i, *ptr, sum=0;
+  n=5;
+  ptr= (int*) malloc(n * sizeof(int));
+  if(ptr==NULL)
+  {
+   exit(0);
+  }
+  for(i=0;i<n;i++)
+   {
+    scanf("%d", ptr+i);
+    
+   }
+   
+   free(ptr);
+   
+}
